09ccbc7004ed4496b541641148d077d8adb09705
Misaki Kasumi <misakikasumi@outlook.com>
2022-04-06 01:56:42 +0800

    now mimalloc

---
+++ mimalloc.cmake
@@ -0,0 +1,15 @@
+set(MI_USE_CXX ON CACHE BOOL "Use the C++ compiler to compile the library (instead of the C compiler)" FORCE)
+
+add_subdirectory(mimalloc EXCLUDE_FROM_ALL)
+
+install(TARGETS mimalloc)
+if(WIN32)
+    install(FILES
+        mimalloc/bin/mimalloc-redirect.dll
+        TYPE BIN
+    )
+    install(FILES
+        mimalloc/bin/mimalloc-redirect.lib
+        TYPE LIB
+    )
+endif()
